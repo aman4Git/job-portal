1e70efb5e087c32747416983c673998a06a9f73b
Aman Mishra <aman.mishra@eleopard.in>
2024-05-14 12:41:15 +0530

    FIX: Minor route related fixes.

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -8,8 +8,9 @@
                 <div class="col">
                     <nav aria-label="breadcrumb" class=" rounded-3 p-3 mb-4">
                         <ol class="breadcrumb mb-0">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Dashboard</li>
+                            <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}">Home</a></li>
+                            <li class="breadcrumb-item"><a href="{{route('admin.users')}}">User</a></li>
+                            <li class="breadcrumb-item active">Edit</li>
                         </ol>
                     </nav>
                 </div>
--- resources/views/admin/users/list.blade.php
+++ resources/views/admin/users/list.blade.php
@@ -8,8 +8,8 @@
                 <div class="col">
                     <nav aria-label="breadcrumb" class=" rounded-3 p-3 mb-4">
                         <ol class="breadcrumb mb-0">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Dashboard</li>
+                            <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}">Home</a></li>
+                            <li class="breadcrumb-item active">Users</li>
                         </ol>
                     </nav>
                 </div>
