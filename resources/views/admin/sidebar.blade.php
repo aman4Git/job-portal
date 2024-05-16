bda2b69bfb6b19aa6059ef65561f7a8359335df9
Aman Mishra <aman.mishra@eleopard.in>
2024-05-16 11:56:17 +0530

    FIX: Admin jobs dashboard route added.

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -9,7 +9,7 @@
                 <a href="{{route('admin.jobs')}}">Jobs</a>
             </li>
             <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                <a href="{{route('account.myJobs')}}">Jobs Applications</a>
+                <a href="{{route('admin.jobApplications')}}">Jobs Applications</a>
             </li>
             <li class="list-group-item d-flex justify-content-between align-items-center p-3">
                 <a href="{{route('account.logout')}}">Logout</a>
